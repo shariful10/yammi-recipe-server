d9ca15ff7d3a9c4a15e1296c523fff04cf4afe7a
Sk Shariful Islam <skshariful10@gmail.com>
2023-05-04 19:35:35 +0600

    Added Some Data On chef-data.json File

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+const category = require("./data/category.json");
+const chefData = require('./data/chef-data.json');
+
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("Server is running");
 });
 
 app.get("/category", (req, res) => {
-    res.send("")
-})
+	res.send(category);
+});
+
+app.get("/chef-data", (req, res) => {
+	res.send(chefData);
+});
 
 app.listen(port, () => {
 	console.log(`API is running on port: ${port}`);
