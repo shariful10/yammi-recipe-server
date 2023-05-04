a827de7a5d5ee75d81d80a3a66375dd57e69d9c7
Sk Shariful Islam <skshariful10@gmail.com>
2023-05-04 14:46:28 +0600

    category.json Added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>{
-    res.send('Server is running')
+app.get("/", (req, res) => {
+	res.send("Server is running");
+});
+
+app.get("/category", (req, res) => {
+    res.send("")
 })
 
-app.listen(port, ()=>{
-    console.log(`API is running on port: ${port}`)
-    
-})
+app.listen(port, () => {
+	console.log(`API is running on port: ${port}`);
+});
